bcf6946f5774685d8da04bc1e12194f6c5416fdd
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-02-05 15:15:08 +0100

    Doc and spec for suits and values

--- lib/solitaire.ex
+++ lib/solitaire.ex
@@ -13,10 +13,17 @@ defmodule Solitaire do
 
   defmodule Cards do
   
+    @type suit :: :hearts | :diamonds | :spades | :clubs
+    @type value :: Range.t(1,13)
+
+    @spec suits :: [ suit ]
+    @doc "List of possible card suits"
     def suits do
       [:hearts, :diamonds, :spades, :clubs]
     end
 
+    @spec values :: [ value ]
+    @doc "List of possible card values"
     def values do
       Enum.to_list(1..13)
     end
