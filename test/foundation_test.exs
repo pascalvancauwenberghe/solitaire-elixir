3898545aee6ccca8422717d269707e50a7d265f1
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-02-07 14:49:54 +0100

    Unit test for empty Foundation

--- test/foundation_test.exs
+++ test/foundation_test.exs
@@ -2,4 +2,11 @@ defmodule FoundationTest do
   use ExUnit.Case
   doctest Solitaire.Foundation
 
+  alias Solitaire.Foundation, as: Foundation
+
+  test "A new Foundation is empty" do
+    foundation = Foundation.new
+
+    assert length(Foundation.up(foundation)) == 0
+  end
 end
