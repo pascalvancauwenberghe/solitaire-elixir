e7f9f99c99e9d8b812e2c8e2e4e64d18ccb31539
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-02-08 17:04:18 +0100

    Refactor test to use shorter aliases for modules

--- test/stock_test.exs
+++ test/stock_test.exs
@@ -2,38 +2,42 @@ defmodule StockTest do
   use ExUnit.Case
   doctest Solitaire.Stock
 
-  test "The initial card contains all down, no up cards" do
-    deck = Solitaire.Deck.new
-    stock = Solitaire.Stock.new(deck)
+  alias Solitaire.Stock, as: Stock
+  alias Solitaire.Deck, as: Deck
+  alias Solitaire.Cards, as: Cards
 
-    assert length(Solitaire.Stock.down(stock)) == 52
-    assert length(Solitaire.Stock.up(stock)) == 0
+  test "The initial card contains all down, no up cards" do
+    deck = Deck.new
+    stock = Stock.new(deck)
+
+    assert length(Stock.down(stock)) == 52
+    assert length(Stock.up(stock)) == 0
   end
 
   test "Turning cards moves them from down to up section" do
-    deck = Solitaire.Deck.new
-    stock = Solitaire.Stock.new(deck)
-    top = List.first(Solitaire.Stock.down(stock))
+    deck = Deck.new
+    stock = Stock.new(deck)
+    top = List.first(Stock.down(stock))
 
-    stock = Solitaire.Stock.turn(stock)
-    assert length(Solitaire.Stock.down(stock)) == 51
-    assert length(Solitaire.Stock.up(stock)) == 1
-    assert List.first(Solitaire.Stock.up(stock)) == top
+    stock = Stock.turn(stock)
+    assert length(Stock.down(stock)) == 51
+    assert length(Stock.up(stock)) == 1
+    assert List.first(Stock.up(stock)) == top
   end
 
   test "The stock is exhausted when all cards are turned over" do
-    deck = Solitaire.Deck.new
-    stock = Solitaire.Stock.new(deck)
+    deck = Deck.new
+    stock = Stock.new(deck)
 
     stock = keep_turning(stock)
-    assert length(Solitaire.Stock.down(stock)) == 0
-    assert length(Solitaire.Stock.up(stock)) == 52
-    assert Solitaire.Stock.exhausted?(stock)
+    assert length(Stock.down(stock)) == 0
+    assert length(Stock.up(stock)) == 52
+    assert Stock.exhausted?(stock)
   end
 
   defp keep_turning(stock) do
-    if length(Solitaire.Stock.down(stock)) > 0 do
-      keep_turning(Solitaire.Stock.turn(stock))
+    if length(Stock.down(stock)) > 0 do
+      keep_turning(Stock.turn(stock))
     else
       stock
     end
