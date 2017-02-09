0cb240662c1600aa5ac36358b7c508777ec787dc
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-02-09 19:20:55 +0100

    Extract Player helper module + excluded skipped and performance tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,59 @@
-ExUnit.start()
+ExUnit.start(exclude: [:skip,:performance])
+
+defmodule Player do
+
+  alias Solitaire.Game, as: Game
+  alias Solitaire.Deck, as: Deck
+
+
+  def play_games(range) do
+     games = for nb <- range do
+       deck = Deck.shuffle(Deck.new,nb)
+       game = Game.new(deck)
+       moves = Game.possible_moves(game)
+       game = play(game,moves)
+       score = Game.score(game)
+       if score == 52 do
+         IO.inspect { nb , score , game }
+       end
+       game
+    end
+    Enum.reduce(games,0,fn(game,score) -> score + Game.score(game) end)
+  end
+
+  def play(game,[]) do
+    if Game.exhausted?(game) do
+      if @log > 0 do
+        show("Final Game::")
+         Game.pretty_print(game) 
+      end
+      game
+    else
+      show("Turning over a card")
+      game = Game.turn(game)
+      moves = Game.possible_moves(game)
+      play(game,moves)
+    end
+  end
+
+  def play(game,[move|_rest]=moves) do
+    show(game,moves) 
+    game = Game.perform(game,move)
+    moves = Game.possible_moves(game)
+    play(game,moves)
+  end
+
+  defp show(game,moves) do
+    if length(moves) > 0 && @log > 0 do
+      show("Game::")
+      Game.pretty_print(game)
+      IO.inspect(moves,label: "Moves")      
+    end
+  end
+
+  defp show(message) do
+    if @log > 0 && String.length(message) > 0, do: IO.puts message
+  end
+
+end
+
